51a4ad4253f5fd5a15b8c7fdcfa1752479b776c7
ritiksingh <ritikkumarsinghdeo6@gmail.com>
2026-06-11 14:53:38 +0530

    add screentshot

--- notification_app_fe/src/api/notificationApi.js
+++ notification_app_fe/src/api/notificationApi.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 export const getNotifications = async () => {
-    console.log("TOKEN:", import.meta.env.VITE_ACCESS_TOKEN);
+  console.log("TOKEN:", import.meta.env.VITE_ACCESS_TOKEN);
+
   try {
+    console.log("API CALLED");
+
     const response = await axios.get(
       "http://4.224.186.213/evaluation-service/notifications",
       {
@@ -12,8 +15,11 @@ export const getNotifications = async () => {
       }
     );
 
+    console.log("STATUS:", response.status);
+    console.log("DATA:", response.data);
+
     return response.data;
   } catch (error) {
-    console.log(error);
+    console.log("ERROR:", error.response?.data || error.message);
   }
 };
